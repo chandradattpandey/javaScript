3646436cfcbab9238e87773cb88b6b64681ec16c
chandradattpandey <pandeyc644@gmail.com>
2022-09-21 18:29:53 +0530

    second commit

---
+++ array.js
@@ -0,0 +1,28 @@
+// arrays
+
+let fruits = ["Apple", "Orange", "Plum"];
+
+console.log(fruits[0]);
+console.log(fruits[1]); 
+console.log(fruits[2]); 
+
+
+//push
+let fruits1 = ["Apple", "Orange", "Plum"];
+fruits1.push('mango');
+console.log(fruits1);
+
+//pop
+let fruits2 = ["Apple", "Orange", "Plum"];
+fruits2.pop();
+console.log(fruits2);
+
+
+// unshit
+fruits2.unshift('mango', 'banana');
+console.log(fruits2);
+
+
+// shift
+fruits2.shift();
+console.log(fruits2);
--- var.js
+++ var.js
@@ -34,32 +34,3 @@ if (a >= 5) {
 console.log(d); // d can not access here 
 console.log(a);
 
-
-// conditional statement
-
-// if statement
-
-let age = 20;
-if (age > 18){
-    console.log('user can vote');
-}
-
-// else-if 
-
-let age2 = 15;
-if (age2 > 18){
-    console.log('user can vote');
-}else{
-    console.log('user can not vote');
-}
-
-// if..else-if
-
-let days = 31;
-if(days <= 7){
-    console.log('it is a weeak');
-}else if((days >=8) && (days <= 15)){
-    console.log('it is a Fortnight');
-}else{
-    console.log('it is a month !!!!!!');
-}
